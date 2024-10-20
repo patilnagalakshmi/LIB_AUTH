77444f9b9003c7f03f0f8114667b04d5ed142124
patilnagalakshmi <140230625+patilnagalakshmi@users.noreply.github.com>
2024-10-20 23:09:36 +0530

    database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # MySQL Database URL with custom port
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:appa@localhost/LIBRARY_MANAGEMENT"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://username:password@localhost/LIBRARY_MANAGEMENT"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
